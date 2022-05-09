450df729a39e70df85a974eb10880cde97e44838
Ray Barnett <rbarnet6@Rays-MBP.hitronhub.home>
2022-05-09 09:07:50 -0700

    image search working in tandem with middleware obscuring Unsplash API

--- src/Search.js
+++ src/Search.js
@@ -1,20 +1,38 @@
 import React, { Component } from "react";
+import ModalImage from "react-modal-image";
+import InfiniteScroll from "react-infinite-scroll-component";
 import './Search.css';
 class Search extends Component {
     state = {
         keyword: '',
-        images: []
+        images: [],
+        page: 2,
+        hasMore: true
     };
     handleInput = event => {
+        this.setState({ images: [] });
         this.setState({ keyword: event.target.value });
     };
     handleClick = () => {
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${this.state.keyword}`).then(res => {
+        if (this.state.keyword.length > 0) {
+            fetch(`http://localhost:8080/search/${this.state.keyword}/1`).then(res => {
+                return res.json();
+            }).then(res => {
+                this.setState({ images: res.results });
+                this.setState({ page: 2 });
+                this.setState({ hasMore: res.results.length >= 21 });
+            });
+        }
+    };
+    loadMore = () => {
+        fetch(`http://localhost:8080/search/${this.state.keyword}/${this.state.page}}`).then((res) => {
             return res.json();
         }).then(res => {
-            this.setState({ images: res.meals });
+            this.setState({ images: this.state.images.concat(res.results) });
+            this.setState({ page: this.state.page + 1 });
+            this.setState({ hasMore: res.results.length >= 21 });
         });
-    };
+    }
     render() {
         return (
             <div>
@@ -27,14 +45,27 @@ class Search extends Component {
                 />
                 <button onClick={this.handleClick}>Search</button>
                 {this.state.images ? (
-                    <div className="results">
-                        {this.state.images.map((image, index) => (
-                            <div className="result" key={index}>
-                                <p>{image.strMeal}</p>
-                                <img src={image.strMealThumb} alt="result" />
-                            </div>
-                        ))}
-                    </div>
+                    <InfiniteScroll
+                        dataLength={this.state.images.length}
+                        next={this.loadMore}
+                        hasMore={this.state.hasMore}
+                        endMessage={
+                            <p style={{ textAlign: 'center' }}>
+                                <b>End Of Search Results</b>
+                            </p>
+                        }
+                    >
+                        <div className="results">
+                            {this.state.images.map((image, index) => (
+                                <div className="result" key={index}>
+                                    <ModalImage
+                                        small={image.urls.thumb}
+                                        large={image.urls.full}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    </InfiniteScroll>
                 ) : (
                     <p>Search Result Empty</p>
                 )}
